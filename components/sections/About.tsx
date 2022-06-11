6708bec9be922e2809da7ef2241b18ec6a89ae52
gustperx <gtgustavo20@gmail.com>
2022-06-11 19:02:12 -0400

    about

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import parse from "html-react-parser";
 import { userLinks } from "../../lib";
 import { Profile } from "../../types";
 import { ContactButtons, SocialMedia } from "../ui";
@@ -12,25 +13,9 @@ export const About: FC<Profile> = ({ description }) => {
             About me
           </h1>
           <p className="mb-5 text-lg font-mono">Hi 👋🏻</p>
-          <p className="mb-5 text-lg font-mono">
-            I am a full stack developer with more than 6 years of experience. I
-            specialize in backend development with PHP and Node.js, currently
-            taking my first steps with Python/Django. I am also an enthusiast of
-            frontend development, designing and offering a good user experience
-            with React.js, Vue.js and Tailwind CSS.
-          </p>
-          <p className="mb-5 text-lg font-mono">
-            I am passionate about technology, continuous learning, project
-            management, team leadership and turning ideas into functional
-            products.
-          </p>
-          <p className="mb-5 text-lg font-mono">
-            With the use of agile methodologies, my experience in project
-            management, software development and my academic background in
-            systems engineering I can help you turn your ideas into functional
-            applications with high value, meet the objectives and expected
-            results in the shortest possible time.
-          </p>
+
+          {parse(description)}
+
           <div className="flex flex-row my-4">
             <SocialMedia {...userLinks.socialMedia} />
           </div>
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,13 +1,36 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
+
+import { Profile } from "../../types";
+import { ClientGraphQL } from "../../lib";
+import { PROFILE_QUERY } from "../../graphql";
+
 import { About } from "../../components/sections";
 import { MainLayout } from "../../components/layouts";
 
-const AboutPage: NextPage = () => {
+interface Props {
+  profile: {
+    user: Profile;
+  };
+}
+
+const AboutPage: NextPage<Props> = ({ profile: { user } }) => {
   return (
     <MainLayout title="Gustavo Perez | About">
-      <About />
+      <About {...user} />
     </MainLayout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const profileData = await ClientGraphQL({
+    query: PROFILE_QUERY,
+  });
+
+  return {
+    props: {
+      profile: profileData,
+    },
+  };
 };
 
 export default AboutPage;
