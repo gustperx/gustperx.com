284726b1b6fddab036ce027fd49de57fb7a59d58
gustperx <gtgustavo20@gmail.com>
2022-06-14 20:06:19 -0400

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <GoogleAnalytics />
       <Component {...pageProps} />
     </ThemeProvider>
   );
